40ced76def7e24e4cf2d6ca691ab37b5e6fc530d
paneltree <paneltree@outlook.com>
2025-05-11 09:57:08 +0800

    remove unused updateFilterRow

--- src/core/WorkspaceManager.h
+++ src/core/WorkspaceManager.h
@@ -64,7 +64,6 @@ public:
     int32_t addFilterToWorkspace(int64_t workspaceId, const FilterData& filter);
     bool removeFilterFromWorkspace(int64_t workspaceId, int32_t filterId);
     std::vector<FilterDataPtr> getFilterDataList(int64_t workspaceId);
-    void updateFilterRow(int64_t workspaceId, int32_t filterId, int32_t filterRow);
     void updateFilterRows(int64_t workspaceId, std::list<int32_t> filterIds);
     void updateFilter(int64_t workspaceId, const FilterData& filter);
     void beginFilterUpdate(int64_t workspaceId);
